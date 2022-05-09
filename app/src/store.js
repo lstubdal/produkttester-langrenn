3ec7c0a941dc2eb53f1b50d880b0fc3411108445
lstubdal <lotte.stubdal@gmail.com>
2022-05-09 20:44:58 +0200

    Remove unused code

--- app/src/store.js
+++ app/src/store.js
@@ -1,75 +1,29 @@
 export default {
     state() {
         return {
-            newTestData: {},
-            tests: [],
-            waxTechWorking: false
+            waxTechWorking: false,
+            /* newTestData: {},
+            tests: [], */
         }
     },
 
     getters: {
-        getTests(state) {
-            return state.tests;
-        },
-
-        getTesterStatus(state) {
-            return state.testerWorking;
-        },
-
-        getwaxTechStatus(state) {
+        getWaxTechStatus(state) {
             return state.waxTechWorking;
         },
 
-        getNewTestData(state) {
-            return state.newTestData;
-        },
-
-        getTestname(state) {
-            return state.name
-        },
-
-        getPlace(state) {
-            return state.place 
-        },
-
-        getTemperature(state) {
-            return state.temperature
-        }
     },
 
     mutations: {
-        setTests(state, tests) {
-            state.tests = tests;
-        },
-
-        setNumberOfSkipairs(state, number) {
-            state.numberOfSkipairs = number
-        },
-
-        setTestname(state, testname) {
-            state.name = testname
-        },
-
-        setPlace(state, place) {
-            state.palace = place
-        },
-
-        setTemperature(state, temperature) {
-            state.temperature = temperature
-        },
-
-        addNewTest(state, test) {
-            state.tests.push(test);
-        },
-
         changeWaxTechStatus(state) {
             return state.waxTechWorking = !state.waxTechWorking;
         }
     },
 
     actions: {
-        updateTests({ commit }, fetchedTests) {
-            commit('setTests', fetchedTests)
+        updateWaxTechStatus({ commit, state}) {
+            commit('changeWaxTechStatus');
+            console.log(state.waxTechWorking)
         },
 
         updateNumberOfSkipairs({ commit }, number) {
@@ -77,24 +31,13 @@ export default {
             // remember local storage
         },
 
-        updateTestname({ commit}, testname ){
-            commit('setTestname', testname);
+        updateTests({ commit }, fetchedTests) {
+            commit('setTests', fetchedTests)
         },
 
-        updatePlace({ commit}, place) {
-            commit('setPlace', place);
-        },
-
-        updateTemperature({commit}, temperature) {
-            commit('setTemperature', temperature)
-        },
-
-        updateWaxTechStatus({ commit, state}) {
-            commit('changeWaxTechStatus');
-            console.log(state.waxTechWorking)
-        },
-        // create muitation (commit)
-        updateNewTestData({ state, commit }, { name, place, date, temperature, snowdata, skipairs }) {
+        
+        // create muitation (commit) ?
+        updateNewTestData({ state}, { name, place, date, temperature, snowdata, skipairs }) {
             state.newTestData = {
                 name: name,
                 place: place,
@@ -103,11 +46,7 @@ export default {
                 snowdata: snowdata,
                 skipairs: skipairs
             }
-
             console.log('new test data', this.newTestData)
-            
-            /* commit('addNewTest', state.newTestData);
-            console.log('tests', this.tests); */
 
         }
 
