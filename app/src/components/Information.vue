fa32b6f479774290571680e15ff6b696a8bdbee0
lstubdal <lotte.stubdal@gmail.com>
2022-05-15 23:34:29 +0200

    Create information comp.

--- app/src/components/Information.vue
+++ app/src/components/Information.vue
@@ -1,14 +1,16 @@
 <template>
-  <section class="information">
+  <section :class="`information ${page === 'previousTest' ? 'information--previousTest' : ''}`">
         <div class="information__place">
-          <img src="/icons/location.svg" alt="location tag">
+          <img v-if="page === 'tester'" src="/icons/location.svg" alt="location tag">
+          <img v-if="page === 'previousTest'" src="/icons/location-red.svg" alt="location tag">
           <span>{{ test.place }}</span>
         </div>
 
-        <h2>{{ test.name }}</h2>
+        <h2 v-if="page === 'tester'">{{ test.name }}</h2>
 
         <span class="information__temperature">{{ test.temperature }} 
-          <img src="/icons/celsius.svg" alt="celsius icon">
+          <img v-if="page === 'tester'" src="/icons/celsius.svg" alt="celsius icon">
+          <img v-if="page === 'previousTest'" src="/icons/celsius-red.svg" alt="celsius icon">
         </span>
     </section>
 </template>
@@ -18,19 +20,29 @@
         props: {
             test: {
                 type: Object 
+            },
+
+            page: {
+                type: String
             }
         }
-
     }
 </script>
 
 <style>
     .information {
+        width: 100vw;
         display: flex;
         justify-content: space-between;
         align-items: center;
         padding: var(--padding-medium);
+        font-family: var(--main-font);
         margin-bottom: var(--margin-large);
+    }
+
+    .information--previousTest {
+        color: var(--second-color);
+        margin-bottom: var(--margin-medium);
     }
 
     .information__place {
@@ -38,7 +50,11 @@
         align-items: center;
     }
 
-    .information__temperature, .information__temperature {
+    .information__place {
+        font-size: 1.3em;
+    }
+
+    .information__temperature {
         font-size: 1.8em;
     }
 
