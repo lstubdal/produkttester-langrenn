dcc56d030fb4f3ec3479ad13c1bb3f4422d46d24
lstubdal <lotte.stubdal@gmail.com>
2022-05-27 11:01:53 +0200

    Sanityclient write access

--- app/src/sanity.js
+++ app/src/sanity.js
@@ -1,9 +1,31 @@
 import sanityClient from '@sanity/client';
-	
+// write to sanity 
+export const writeClient = sanityClient({
+    projectId: '4rt7aync',
+    dataset: 'production',
+    token: import.meta.env.VITE_SANITY,
+    //token: process.env.SANITY_AUTH_TOKEN,
+    useCdn: false,
+    withCredentials: true
+  })
+
+// fetch from sanity
+export const readClient = sanityClient({
+projectId: '4rt7aync',
+dataset: 'production',
+useCdn: true
+})
+
+
+/*
+// fetching from sanity
 export default sanityClient({ 
     projectId: '4rt7aync', // unique project id
     dataset: 'production',
     apiVersion: '2022-05-04', 
-    useCdn: 'false', // false local / true netlify
+    useCdn: 'true', // false local / true netlify
     withCredentials: true // for sanity muations
 })
+ */
+
+
