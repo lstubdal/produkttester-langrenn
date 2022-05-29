835187012a7894944aaea64ee8508c945204038f
lstubdal <lotte.stubdal@gmail.com>
2022-05-29 10:09:17 +0200

    Update to most used pairs

--- studio/schemas/documents/numberOfSkipairs.js
+++ studio/schemas/documents/numberOfSkipairs.js
@@ -1,8 +1,6 @@
+// Most used number of pairs used in testing
 export default [
     { title: 2, value: 2},
     { title: 4, value: 4},
-    { title: 6, value: 6},
     { title: 8, value: 8},
-    { title: 10, value: 10},
-    { title: 12, value: 12},
 ]
